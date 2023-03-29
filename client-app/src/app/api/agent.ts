1e6e18bfea9c96b5dcb9125bd20352658183ae70
Minh giang trương <minhgiang241@gmail.com>
2023-03-30 00:05:59 +0700

    finish add client profile photo

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -7,7 +7,7 @@ import { history } from "../../index";
 import { Activity } from "../models/activity";
 import { User } from "../models/user";
 import { store } from "../stores/store";
-import { Profile } from "../models/profile";
+import { Photo, Profile } from "../models/profile";
 
 const sleep = (delay: number) => {
   return new Promise((resolve) => setTimeout(resolve, delay));
@@ -95,7 +95,16 @@ const Account = {
 };
 
 const Profiles = {
-  get: (username: string) => requests.get<Profile>(`/profiles/${username}`)
+  get: (username: string) => requests.get<Profile>(`/profiles/${username}`),
+  uploadPhoto: (file: Blob) => {
+    let formData = new FormData();
+    formData.append('File', file);
+    return axios.post<Photo>('photos', formData, {
+      headers: { 'Content-type': 'multipart/form-data' },
+    })
+  },
+  setMainPhoto: (id: String) => requests.post(`/photos/${id}/setMain`, {}),
+  deletePhoto: (id: string) => requests.del(`/photos/${id}`)
 }
 
 const agent = {
--- client-app/src/features/profiles/ProfilePhotos.tsx
+++ client-app/src/features/profiles/ProfilePhotos.tsx
@@ -1,8 +1,8 @@
 import { observer } from 'mobx-react-lite'
-import React, { useState } from 'react'
+import { SyntheticEvent, useState } from 'react'
 import { Card, Header, Tab, Image, Grid, Button } from 'semantic-ui-react'
 import PhotoUploadWidget from '../../app/common/imageUpload/PhotoUploadWidget';
-import { Profile } from '../../app/models/profile';
+import { Photo, Profile } from '../../app/models/profile';
 import { useStore } from '../../app/stores/store';
 
 interface Props {
@@ -10,8 +10,20 @@ interface Props {
 }
 
 export default observer(function ProfilePhotos({ profile }: Props) {
-  const { profileStore: { isCurrentUser } } = useStore()
+  const { profileStore: { isCurrentUser, setMainPhoto, uploadPhoto, uploading, loading, deletePhoto } } = useStore()
   const [addPhotoMode, setAddPhotoMode] = useState(false)
+  const [target, setTarget] = useState('')
+  function handlePhotoUpload(file: Blob) {
+    uploadPhoto(file).then(() => setAddPhotoMode(false))
+  }
+  function handleSetMainPhoto(photo: Photo, e: SyntheticEvent<HTMLButtonElement>) {
+    setTarget(e.currentTarget.name)
+    setMainPhoto(photo)
+  }
+  function handleDeletePhoto(photo: Photo, e: SyntheticEvent<HTMLButtonElement>) {
+    setTarget(e.currentTarget.name)
+    deletePhoto(photo)
+  }
   return (
     <Tab.Pane>
       <Grid>
@@ -22,12 +34,33 @@ export default observer(function ProfilePhotos({ profile }: Props) {
           />)}
         </Grid.Column>
         <Grid.Column width={16}>
-          {addPhotoMode ? (< PhotoUploadWidget />) :
+          {addPhotoMode ? (< PhotoUploadWidget uploadPhoto={handlePhotoUpload} loading={uploading} />) :
             <Card.Group itemsPerRow={5}>
 
               {profile.photos?.map(photo => (
                 <Card key={photo.id}>
                   <Image src={photo.url} />
+                  {isCurrentUser && (
+                    <Button.Group fluid widths={2}>
+                      <Button
+                        basic
+                        color='green'
+                        content='Main'
+                        name={'main' + photo.id}
+                        disabled={photo.isMain}
+                        loading={target === 'main' + photo.id && loading}
+                        onClick={e => handleSetMainPhoto(photo, e)} />
+
+                      <Button
+                        basic
+                        name={photo.id}
+                        loading={target == photo.id && loading}
+                        disabled={photo.isMain}
+                        onClick={e => handleDeletePhoto(photo, e)}
+                        color='red'
+                        icon='trash' />
+                    </Button.Group>
+                  )}
                 </Card>
               ))
               }
